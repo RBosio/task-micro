ef62b19e8a43d1d85543b8ca972b7cbe975ef081
Fiido <roccobosio@gmail.com>
2024-04-14 20:52:08 -0300

    typeorm config

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MYSQL_URI: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- apps/task/src/task.module.ts
+++ apps/task/src/task.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MYSQL_URI: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [TaskController],
   providers: [TaskService],
 })
